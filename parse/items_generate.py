520fd833fc6d86f87e4f539069a76354e746f071
abzaloid <abzal.serekov@gmail.com>
2015-02-23 13:47:39 +0600

    updated

--- parse/items_generate.py
+++ parse/items_generate.py
@@ -1,36 +1,52 @@
 import random
 import json
 
-f = open("items.txt", "r")
 
-items = []
+def generate_items():
+	f = open("items.txt", "r")
 
-for item in f:
-	items.append(item[:-1])
+	items = []
+
+	for item in f:
+		items.append(item[:-1])
 
 
-f.close()
+	f.close()
 
-category = ['First Type', 'Second Type', 
-			'Third Type', 'Fourth Type']
+	category = ['First Type', 'Second Type', 
+				'Third Type', 'Fourth Type']
 
-stores = ['Green', 'Anvar', 'Gross', 'Ramstore', 'Galomart']
+	stores = ['Green', 'Anvar', 'Gross', 'Ramstore', 'Galomart']
 
-items_json = []
+	items_json = []
 
-for item in items:
-	cur_item = {}
-	for store_id in range(len(stores)):
-		cur_item["name"] = item
-		cur_item["category"] = random.choice(category)
-		cur_item["store_id"] = store_id
-		cur_item["price"] = random.uniform(50, 1000)
-		cur_item["image"] = "http://velocityagency.com/wp-content/uploads/2013/08/go.jpg"
-		cur_item["description"] = "description goes here"
-	items_json.append(cur_item)
+	for item in items:
+		cur_item = {}
+		for store_id in range(len(stores)):
+			cur_item["name"] = item
+			cur_item["category"] = random.choice(category)
+			cur_item["store_id"] = store_id
+			cur_item["price"] = random.uniform(50, 1000)
+			cur_item["image"] = "http://velocityagency.com/wp-content/uploads/2013/08/go.jpg"
+			cur_item["description"] = "description goes here"
+		items_json.append(cur_item)
 
-f = open("items.json", "w")
+	f = open("items.json", "w")
 
-f.write(json.dumps(items_json))
+	f.write(json.dumps(items_json))
 
-f.close()
+	f.close()
+
+def get_with_commas():
+	f = open("dict.txt", 'r')
+	my_list = ''.join(line[:-1] for line in f).split(' ')
+	f.close()
+	f = open('items.txt', 'w')
+	for item in my_list:
+		if len(item[:-1]) > 0:
+			f.write("%s\n" % item[:-1])
+	f.close()
+
+
+get_with_commas()
+generate_items()
