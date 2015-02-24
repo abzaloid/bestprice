f33bdce00b0a085cf85c4ab62c96e46144251160
unknown <User@4WBG65J.nu.edu.kz>
2015-02-24 10:37:10 +0600

    organization of code

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from webapp2_extras import sessions
 from google.appengine.api import mail
 
 
+import databaseOperations
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
@@ -22,20 +24,6 @@ config = {}
 config['webapp2_extras.sessions'] = {
     'secret_key': 'my-super-secret-key',
 }
-
-### LEVENSHTEIN DISTANCE ###
-# returns levenshtein distance (if same then 0, otherwise greater than 0)
-def levenshtein(seq1, seq2):
-    oneago = None
-    thisrow = range(1, len(seq2) + 1) + [0]
-    for x in xrange(len(seq1)):
-        twoago, oneago, thisrow = oneago, thisrow, [0] * len(seq2) + [x + 1]
-        for y in xrange(len(seq2)):
-            delcost = oneago[y] + 1
-            addcost = thisrow[y - 1] + 1
-            subcost = oneago[y - 1] + (seq1[x] != seq2[y])
-            thisrow[y] = min(delcost, addcost, subcost)
-    return thisrow[len(seq2) - 1]
 
 ### MODELS ###
 class Item(db.Model):
@@ -154,31 +142,6 @@ class AnotherMainPage(Handler):
     def get(self):
         self.render("main2.html")
 
-class SearchItem(Handler):
-    def get(self):
-        self.render("search.html")
-
-    def post(self):
-        max_distance = 2
-        self.searching_object = self.request.get('searching_object')
-        items = list(db.GqlQuery("SELECT * FROM Item"))
-        stores = list(db.GqlQuery("SELECT * FROM Store"))
-        found_items = []
-        for item in items:
-            cur_distance = levenshtein(item.name, self.searching_object)
-            if cur_distance <= max_distance:
-                logging.debug(cur_distance)
-                logging.debug(item.name)
-                found_items.append((item, cur_distance))
-        found_items.sort(key=lambda tup: tup[1])
-
-        found_stores = []
-        for store in stores:
-            if store.name == self.searching_object:
-                found_stores.append(store)
-        self.render("search.html", searching_object = self.searching_object,
-                                items = list(tup[0] for tup in found_items), 
-                                stores = found_stores)
 
 class JSONHandler(Handler):
     def get(self):
@@ -314,33 +277,6 @@ class EditItemHandler(Handler):
         tshirt.put()
         self.redirect('/item/edit')
 
-class UpdateDatabase(Handler):
-    def get(self):
-        f = open("items.json", "r")
-        items = json.loads(''.join(line for line in f))
-        f.close()
-        for item in items:
-            t = Item(name = item["name"], 
-                       category = item["category"],
-                       price = item["price"],
-                       description = item["description"],
-                       image = item["image"],
-                       storeid = item["store_id"])
-            t.put()
-        
-        f = open("stores.json", "r")
-        stores = json.loads(''.join(line for line in f))
-        f.close()
-        for store in stores:
-            t = Store(name = item["name"], 
-                       description = item["description"])
-            t.put()
-
-class ShowDatabase(Handler):
-    def get(self):
-        items = list(db.GqlQuery("SELECT * FROM Item"))
-        self.render("main.html", items = items)
-
 class EmailHandler(Handler):
     def get(self):
         sender_address = "prakhar1989@gmail.com"
@@ -372,6 +308,6 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                ('/listorders', ListOrdersHandler),
                                ('/secure', SecureHandler),
                                ('/tshirt/(\d+)', ShowItemHandler),
-                               ('/update_database', UpdateDatabase),
-                               ('/show_database', ShowDatabase),
+                               ('/update_database', databaseOperations.UpdateDatabase),
+                               ('/show_database', databaseOperations.ShowDatabase),
                                ('/search', SearchItem)], config=config, debug=True)
