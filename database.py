0a4d0378bca302083b9be4599d2a3fc455c3fcf9
unknown <User@4WBG65J.nu.edu.kz>
2015-02-25 12:28:56 +0600

    check if model already exists -> no need to update

--- database.py
+++ database.py
@@ -9,25 +9,38 @@ import models
 
 class UpdateDatabase(handler.Handler):
     def get(self):
-        f = open("items.json", "r")
-        items = json.loads(''.join(line for line in f))
-        f.close()
-        for item in items:
-            t = models.Item(name = item["name"], 
-                       category = item["category"],
-                       price = item["price"],
-                       description = item["description"],
-                       image = item["image"],
-                       storeid = item["store_id"])
-            t.put()
+
+        if db.Query(models.Item).count() <= 0:
+            f = open("parse/items.json", "r")
+            items = json.loads(''.join(line for line in f))
+            f.close()
+            for item in items:
+                t = models.Item(name = item["name"], 
+                           category = item["category"],
+                           price = item["price"],
+                           description = item["description"],
+                           image = item["image"],
+                           storeid = item["store_id"])
+                t.put()
         
-        f = open("stores.json", "r")
-        stores = json.loads(''.join(line for line in f))
-        f.close()
-        for store in stores:
-            t = models.Store(name = item["name"], 
-                       description = item["description"])
-            t.put()
+        if db.Query(models.Store).count() <= 0:
+            f = open("parse/stores.json", "r")
+            stores = json.loads(''.join(line for line in f))
+            f.close()
+            for store in stores:
+                t = models.Store(name = item["name"], 
+                           description = item["description"])
+                t.put()
+
+        if db.Query(models.Category).count() <= 0:
+            f = open("parse/categories.json")
+            categories = json.loads(''.join(line for line if f))
+            f.close()
+            for category in categories:
+                t = models.Category(name = category["name"])
+                t.put()
+                
+        self.redirect('/')
 
 class ShowDatabase(handler.Handler):
     def get(self):
