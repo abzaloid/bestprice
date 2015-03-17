2baffbec68fb6e193b153647d49c267979bae4e2
unknown <abzal.serekov@gmail.com>
2015-03-17 17:23:20 +0600

    small amendment for better look

--- controllers.py
+++ controllers.py
@@ -68,7 +68,15 @@ class ShowCategoryWithPaginationHandler(handler.Handler):
         item_cart = self.session.get('items')
         logging.error(total_items_size)
         logging.error(current_page)
-        self.render("main.html", current_page=int(current_page), pagination_count=pagination_count, items_per_page=items_per_page, total_items_size=total_items_size, items=items, items_size=len(items)-1, categories=categories, item_cart=item_cart, cat_num=int(category_id))
+        self.render("main.html", current_page=int(current_page), 
+            pagination_count=pagination_count, 
+            items_per_page=items_per_page, 
+            total_items_size=total_items_size, 
+            items=items, 
+            items_size=len(items)-1, 
+            categories=categories, 
+            item_cart=item_cart, 
+            cat_num=int(category_id))
 
 
 class ShowCategoryHandler(handler.Handler):
@@ -92,7 +100,15 @@ class ShowCategoryHandler(handler.Handler):
         items = items[:min(len(items),items_per_page)]
         categories = list(caching.get_categories())
         item_cart = self.session.get('items')
-        self.render("main.html", current_page=0, pagination_count=pagination_count, items_per_page=items_per_page, total_items_size=total_items_size, items=items, items_size=len(items)-1, categories=categories, item_cart=item_cart, cat_num=int(category_id))
+        self.render("main.html", current_page=0, 
+            pagination_count=pagination_count, 
+            items_per_page=items_per_page, 
+            total_items_size=total_items_size, 
+            items=items, 
+            items_size=len(items)-1, 
+            categories=categories, 
+            item_cart=item_cart, 
+            cat_num=int(category_id))
 
 class ShowItemHandler(handler.Handler):
     def get(self, item_id):
