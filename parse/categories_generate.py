207d51f4811f93ddca1e54bc5bf4ae8be0bad845
unknown <abzal.serekov@gmail.com>
2015-03-18 17:02:37 +0600

    small bug removed

--- parse/categories_generate.py
+++ parse/categories_generate.py
@@ -4,14 +4,14 @@ import random
 import json
 categories = [u'Бакалея',
 			u'Чай. Кофе. Какао',
-			u'Молочные продукты',
+			u'Молочные Продукты',
 			u'Напитки']
 
 subcategories = {}
 subcategories[u'Бакалея'] = [u'Крупы',u'Макаронные Изделия',u'Соль',u'Сахар']
 subcategories[u'Чай. Кофе. Какао'] = [u'Чай']
 subcategories[u'Напитки'] = [u'Напитки']
-subcategories[u'Молочные продукты'] = [u'Молоко',u'Майонез']
+subcategories[u'Молочные Продукты'] = [u'Молоко',u'Майонез']
 
 
 subcategories_ = [u'Соль',
