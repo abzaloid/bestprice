22b350642eb40c12a9ef047eb27ae5075e86af3f
abzaloid <abzal.serekov@gmail.com>
2015-03-25 09:22:09 +0600

    small bug removed

--- js/application.js
+++ js/application.js
@@ -98,36 +98,34 @@ $(function(){
       new Messi(item_description + "<br/> <img src='"+item_image+"' width='150' height='150' style='margin:auto;'/><hr/>Цена " + item_price + "тг. ", {title: item_name, modal: true} );
     }
     function removeItem(class_name, _id, item_name_safe) {
-      $("."+class_name).remove();
+        $("."+class_name).remove();
 
-      last_quantity = $(".addtocart"+_id).text();
+        var last_quantity = $(".addtocart"+_id).text();
 
-     cur = "Добавить"
-     $(".addtocart"+_id).text(cur);
-     $(".plus"+_id).hide();
-     $(".minus"+_id).hide();
+        cur = "Добавить"
+        $(".addtocart"+_id).text(cur);
+        $(".plus"+_id).hide();
+        $(".minus"+_id).hide();
 
-     $.ajax({
-        type: "POST",
-        url: "/delitem",
-        dataType: 'json',
-        data: JSON.stringify({"item": item_name_safe, "last_quantity" : last_quantity})
-      })
-      .always(function (data) {
-        if (data["status"] == 0) {
-          alert("Войдите в систему, чтобы совершить покупку");
-        } else {
-            var qnt = parseInt(data["number"]);
-            $(".itemCount").text(qnt.toString());
-            for (var key in data) {
-              if (key.indexOf("0123456789")) {
-                $(".my_store" + key).text(data[key].toString() + " тг");
-              }
+        $.ajax({
+           type: "POST",
+           url: "/delitem",
+           dataType: 'json',
+           data: JSON.stringify({"item": item_name_safe, "last_quantity" : last_quantity})
+         })
+        .always(function (data) {
+           if (data["status"] == 0) {
+             alert("Войдите в систему, чтобы совершить покупку");
+           } else {
+               var qnt = parseInt(data["number"]);
+               $(".itemCount").text(qnt.toString());
+               for (var key in data) {
+                 if (key.indexOf("0123456789")) {
+                   $(".my_store" + key).text(data[key].toString() + "тг");
+                 }
+               }
             }
-
-
-        }
-      });                             
+          });                             
 
 
       // item._id
